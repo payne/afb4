47a952aca539391fabe0399800c057f51e1d031d
Matt Payne <Payne@MattPayne.org>
2022-03-27 17:33:32 -0500

    ng add @angular/fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-e9311',
+    appId: '1:537221498866:web:1428345f0b472ff9ff8d6b',
+    databaseURL: 'https://kanbanfire-e9311-default-rtdb.firebaseio.com',
+    storageBucket: 'kanbanfire-e9311.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAtZ-bsrWSeaxmgYonh17hcJ-qzscn9o0Q',
+    authDomain: 'kanbanfire-e9311.firebaseapp.com',
+    messagingSenderId: '537221498866',
+    measurementId: 'G-SY27T79XHP',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-e9311',
+    appId: '1:537221498866:web:1428345f0b472ff9ff8d6b',
+    databaseURL: 'https://kanbanfire-e9311-default-rtdb.firebaseio.com',
+    storageBucket: 'kanbanfire-e9311.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAtZ-bsrWSeaxmgYonh17hcJ-qzscn9o0Q',
+    authDomain: 'kanbanfire-e9311.firebaseapp.com',
+    messagingSenderId: '537221498866',
+    measurementId: 'G-SY27T79XHP',
+  },
   production: false
 };
 
